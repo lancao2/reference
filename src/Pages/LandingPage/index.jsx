948f6be1d957cb3c1e646bf879199f9e4630519b
lancao2 <lancao.af@gmail.com>
2022-06-07 15:46:31 -0300

    feat: landingPage static

--- src/Pages/LandingPage/index.jsx
+++ src/Pages/LandingPage/index.jsx
@@ -1,5 +1,26 @@
-
+import "./styles.css";
+import BG from "../../imgs/referenceBG.jpg";
+import RippedPaper from "../../imgs/ripped-paper.png";
 const LandingPage = () => {
-
-}
-export default LandingPage
+  return (
+    <div id="landingPage">
+      <div className="main">
+        <div className="text">
+          <h2>COMFERÊNCIA</h2>
+          <h1>Reference</h1>
+          <h3>LUCAS 21: 20-22</h3>
+        </div>
+        <button>Eu Vou</button>
+        <span>Rua vellho Cassiano 132, Palhada</span>
+      </div>
+      <img className="rippedPaperTop" src={RippedPaper} alt="Papel rasgado" />
+      <img
+        className="rippedPaperBottom"
+        src={RippedPaper}
+        alt="Papel rasgado"
+      />
+      <img className="backGround" src={BG} alt="backGround reference" />
+    </div>
+  );
+};
+export default LandingPage;
--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -4,11 +4,19 @@ import LandingPage from "../Pages/LandingPage";
 import Complete from "../Pages/Complete";
 
 const Routes = () => {
+  return(
     <Switch>
-        <Route exact path="/"><LandingPage/></Route>
-        <Route exact path="/Form_Reference"><FormReference/></Route>
-        <Route exact path="/"><Complete/></Route>
+      <Route path="/">
+        <LandingPage />
+      </Route>
+      <Route exact path="/form_reference">
+        <FormReference />
+      </Route>
+      <Route exact path="/complete">
+        <Complete />
+      </Route>
     </Switch>
-}
+  );
+};
 
-export default Routes
+export default Routes;
